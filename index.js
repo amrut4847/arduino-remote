9087619cfce8025850e0f4408f7e33fd98c69e2d
Amrut <kshirsagar_amrut@yahoo.com>
2015-12-14 23:35:10 +0530

    make it better

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+
+  //response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
