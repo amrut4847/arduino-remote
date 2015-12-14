082935f53430ceae8ec02fa1b708250a278d812e
Amrut <kshirsagar_amrut@yahoo.com>
2015-12-14 23:47:06 +0530

    make it better

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var five = require("johnny-five");
+var EtherPort = require("etherport");
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -10,7 +12,14 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-
+var board = new five.Board({ 
+  port: new EtherPort(3030) 
+});
+ 
+board.on("ready", function() {
+  var led = new five.Led(8);
+  led.blink(500);
+})
   response.render('pages/index');
 });
 
