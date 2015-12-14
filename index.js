149ccfa7a89bd8eb02829c5c5735d7c284074450
Amrut <kshirsagar_amrut@yahoo.com>
2015-12-14 23:54:24 +0530

    make it better

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-var board = new five.Board({ 
+/*var board = new five.Board({ 
   port: new EtherPort(3030) 
 });
  
 board.on("ready", function() {
   var led = new five.Led(8);
   led.blink(500);
-})
+})*/
   response.render('pages/index');
 });
 
