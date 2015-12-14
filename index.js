8aeb1117b02a3e8542c5cbd3220813124162d260
Amrut <kshirsagar_amrut@yahoo.com>
2015-12-14 23:42:27 +0530

    make it better

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
 
-  //response.render('pages/index');
+  response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
