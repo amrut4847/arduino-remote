277d9d0102d336b7380f68236bc1302bfcb92ec1
Amrut <kshirsagar_amrut@yahoo.com>
2016-01-01 17:07:32 +0530

    make it better

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var express = require('express');
-/*var five = require("johnny-five");
-var EtherPort = require("etherport");*/
+var five = require("johnny-five");
+var EtherPort = require("etherport");
 var app = express();
+var bodyParser = require('body-parser');
 
+var board = new five.Board({ 
+    port: new EtherPort(3030) 
+});
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
-
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+	extended: true
+}));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -22,6 +27,23 @@ board.on("ready", function() {
 })*/
   response.render('pages/index');
 });
+
+board.on("ready", function() {
+    var led = new five.Led(8);
+	app.post('/togglebtn/on', function(request, response) {
+		var check = request.body;
+		console.log(check.key);
+		led.on();
+		response.send({"msg":check.key});
+	});
+
+	app.post('/togglebtn/off', function(request, response) {
+		var check = request.body;
+		console.log(check.key);
+		led.off();
+		response.send({"msg":check.key});
+	});
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
