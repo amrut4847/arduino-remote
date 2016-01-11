6aebd6de7b4a958c93d6f5b58ac4edebc28c831f
Amrut <kshirsagar_amrut@yahoo.com>
2016-01-11 23:43:31 +0530

    make it better

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 var express = require('express');
-var five = require("johnny-five");
-var EtherPort = require("etherport");
+var five = require("./lib/johnny-five");
+//var EtherPort = require("etherport");
 var app = express();
 var bodyParser = require('body-parser');
 
-var board = new five.Board({ 
-    port: new EtherPort(3030) 
-});
+var board = new five.Board({ port : '\\\\.\\COM8' });
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
