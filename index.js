21a82932e68586685bea006048c1a753382a8784
Amrut <kshirsagar_amrut@yahoo.com>
2016-01-12 00:05:55 +0530

    make it better

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var five = require("johnny-five");
-//var EtherPort = require("etherport");
+var EtherPort = require("etherport");
 var app = express();
 var bodyParser = require('body-parser');
 
-var board = new five.Board({ port : '\\\\.\\COM8' });
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
@@ -25,7 +24,7 @@ board.on("ready", function() {
 })*/
   response.render('pages/index');
 });
-
+var board = new five.Board({ port : '\\\\.\\COM8' });
 board.on("ready", function() {
     var led = new five.Led(8);
     console.log("Board Ready");
